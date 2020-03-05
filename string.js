b736e4966bc3e87d64c229ceda820733ad63c7b2
Andrew5205 <andrewchung11@gmail.com>
2020-03-04 17:02:51 -0800

    update string.js - slice() trim() search()

--- string.js
+++ string.js
@@ -2,6 +2,9 @@
 // str.length
 // str.split() - String.prototype 
 // arr.join() - Array.prototype 
+// str.slice() - String.prototype
+// str.trim() - String.prototype
+// str.search() - String.prototype
 // str.charCodeAt() - String.prototype
 // str.toUpperCase() - String.prototype
 
@@ -9,7 +12,9 @@
 // Remove blanks
 // Get string digits 
 // Acronyms
-
+// Count Non-Spaces
+// Remove Shorter Strings
+// String: Reverse
 
 
 // Strings are immutable
@@ -39,15 +44,33 @@ console.log(wordArray[3].split(" "));    // [ 'you' ]
 
 
 // join() - Array.prototype 
-const wordArray = ["hi", "how", "are", "you"];
-console.log(wordArray.join());  //hi,how,are,you    // "," is used by default 
-console.log(wordArray.join(''));  //hihowareyou
-console.log(wordArray.join("-"));  //hi-how-are-you
-console.log(wordArray.join(" "));  //hi how are you
+const wordArray2 = ["hi", "how", "are", "you"];
+console.log(wordArray2.join());  //hi,how,are,you    // "," is used by default 
+console.log(wordArray2.join(''));  //hihowareyou
+console.log(wordArray2.join("-"));  //hi-how-are-you
+console.log(wordArray2.join(" "));  //hi how are you
 
 
 console.log(1+2+"3"+'4'+5);    // 3345
 
+
+
+// slice() String.prototype
+const string = "hello, how are you";
+console.log(string.slice(2,10));
+
+
+
+// trim() - String.prototype
+// remove whitespace (spaces, tabs, newlines) from both sides, and return a new string
+const myString = "       this string is going to be trimed         ";
+console.log(myString.trim());   // this string is going to be trimed
+
+
+
+// search() - String.prototype
+const beSearchedString = "how are you doing today";
+console.log(beSearchedString.search("you"));    // return index of staring char 8
 
 
 // toUpperCase() - String.prototype 
@@ -123,3 +146,76 @@ console.log(acronyms(""));
 console.log(acronyms("there's no free lunch - gotta pay yer way."));
 
 /* ************************************************************************************************************************ */
+
+
+
+// Count Non-Spaces
+// Accept a string and return the number of non-space characters found in the string. 
+// For example, given "Honey pie, you are driving me crazy", return 29 (not 35).
+function countNonSpace(str) {
+    var count = 0;
+    var arr = str.split("");
+    for (var i = 0; i < str.length; i++) {
+        if (arr[i].charCodeAt() != 32) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countNonSpace("Honey pie, you are driving me crazy"));
+
+/* ************************************************************************************************************************ */
+
+
+
+// Remove Shorter Strings
+// Given a string array and value (length), remove any strings shorter than length from the array
+function removeShorterString(arr, int) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].length > int) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+console.log(removeShorterString(['abcd', 'abcdef', 'abcdefgh', 'abc', 'a', 'abcdefghij', 'abcdefg'], 3));
+console.log(removeShorterString(['11','2','345','4','5'], 1));
+
+/* ************************************************************************************************************************ */
+
+
+
+// String: Reverse
+// Implement reverseString(str) that, given string, returns that string with characters reversed.
+// Given "creature", return "erutaerc". Tempting as it seems, do not use the built-in reverse()!
+function stringReverse(str) {
+    var arr = str.split("");
+    var temp = " ";
+    for ( var i = 0; i <= arr.length/ 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 -i] = temp;
+        // console.log(arr[i]);
+    }
+    return arr;
+}
+console.log(stringReverse('abcdef'));
+// console.log(stringReverse(['a', 'b', 'c', 'd', 'e']));
+
+
+function reverseArray(arr) {
+    for (var i = 0; i <= arr.length / 2; i++) {
+        var temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = temp;
+    }
+    return arr;
+}
+console.log(reverseArray([1,2,3,4,5,6,7]));
+
+/* ************************************************************************************************************************ */
+
+
+
+
