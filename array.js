ff4f60577879079b0ff5936bf7b8f495a0c34a8f
Andrew5205 <andrewchung11@gmail.com>
2020-04-05 14:49:50 -0700

    Add on array.js shift()

--- array.js
+++ array.js
@@ -12,6 +12,7 @@
 // insertAt() - 
 // removeAt() - 
 // swap() - 
+// shift() - Array.prototype
 // 
 // 
 // /* ************************************ */
@@ -163,7 +164,16 @@ function swap(int1, int2) {
 console.log(swap(10, 20));
 
 
-console.log("**************************")
+// shift() - Array.prototype
+// removes the first element from an array and returns that removed element. 
+// This method changes the length of the array.
+
+let arrShift = [1,2,3,4,5];
+console.log(arrShift.shift());       // 1
+console.log(arrShift);               // [ 2, 3, 4, 5 ]
+
+
+console.log("*********** RemoveDup  ***************")
 /* ****************************************************************************************** */
 // Remove Duplicates
 // Given a sorted array, remove duplicate values. 
@@ -263,43 +273,3 @@ function shiftToRight(arr) {
 }
 
 console.log(shiftToRight([9,8,7,6,5,4,3]));     // [9,9,8,7,6,5,4,3]
-
-
-// ***************************************************************************************** //
-
-
-function mergeArray( arr1, arr2) {
-    let arr3 = arr1.concat(arr2);
-    return arr3.sort();
-    // return arr1.concat(arr2).sort();
-}
-
-console.log(mergeArray([1,2,3,4,5], [2,4,6,8,9]));  // // [ 1, 2, 2, 3, 4, 4, 5, 6, 8, 9 ]
-
-
-// ***************************************************************************************** //
-
-function mergeTwo(arr1, arr2) {
-    let output = [];
-    let i = 0;
-    let j = 0;
-    let current = 0;
-
-    while (current < (arr1.length + arr2.length)) {
-
-        if (arr1[i] < arr2[j]) {
-            output[current] = arr1[i];
-            i++;
-        } else {
-            output[current] = arr2[j];
-            j++;
-        }
-        current++;
-    }
-    return output;
-}
-
-console.log(mergeTwo([1,2,3,4,5], [2,4,6,8,9]));    // [ 1, 2, 2, 3, 4, 4, 5, 6, 8, 9 ]
-
-
-
